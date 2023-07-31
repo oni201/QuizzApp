bb50e0207bf9c1191aea02dd53620a3141d83660
oni201 <jucse28.356@gmail.com>
2023-07-31 20:17:45 +0600

    QuizzApp

---
+++ app/src/main/java/com/example/q/QuestionLibrary.java
@@ -0,0 +1,44 @@
+package com.example.q;
+
+public class QuestionLibrary {
+    private String mQuestions [] ={
+            "Which part of the plants holds it in the soil?",
+            "Which part of the plant absorbs energy from the sun.",
+            "Which part of the plant attracts bees, butterflies and hummingbirds.",
+            "The _ holds the plant upright."
+    };
+    private  String mChoices [][] = {
+            {"Roots","Stem","Flower"},
+            {"Fruits","Leaves","Seeds"},
+            {"Bark","Flower","Roots"},
+            {"Fruits","Leaves","Seeds"}
+    };
+
+    private String mCorrectAnswers []={"Roots","Leaves","Flower","Seeds"};
+
+    public String getQuestion(int a){
+        String question=mQuestions[a];
+        return question;
+    }
+
+    public String getChoice1(int a){
+        String choice0=mChoices[a][0];
+        return choice0;
+    }
+    public String getChoice2(int a){
+        String choice1=mChoices[a][1];
+        return choice1;
+    }
+    public String getChoice3(int a){
+        String choice2=mChoices[a][2];
+        return choice2;
+    }
+    public String getChoice4(int a){
+        String choice3=mChoices[a][2];
+        return choice3;
+    }
+    public String getCorrectAnswer(int a) {
+        String answer = mCorrectAnswers[a];
+        return answer;
+    }
+}
